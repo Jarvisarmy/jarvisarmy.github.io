48459836577737400f7047ceb3f3b1609468e8c2
Jarvisarmy <1102297439zzd@gmail.com>
2020-09-07 03:29:13 -0400

    using vue.js
    using Vue as framework

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <a href="./assets/assets.html"> previous website built without vue.js</a>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
