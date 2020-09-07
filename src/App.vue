405662c2bcd5272aa16812e28ada7d50ac0f8ff3
Jarvisarmy <1102297439zzd@gmail.com>
2020-09-07 05:29:36 -0400

    entirely update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         <h2> hello </h2>
-        <p> I'm currently working on my personal website using vue.js. the following links are the project I done previously without vue.js</p>
-        <a href="./assets/tetris/tetris.html"> tetris</a>
-        <a href="./assets/schedule/schedule.html"> schedule</a>
-        <a href="./assets/color/colorGame.html"> colorGame </a>
+        <p> I am currently working my personal website using vue.js, the following links are the projects I done without using vue.js </p>
+        <a href="./../assets/tetris/tetris.html"> tetris </a>
+        <a href="./../assets/schedule/schedule.html"> schedule </a>
+        <a href="./../assets/color/colorGame.html"> colorGame </a>
+        
     </div>
+
+
+
 </template>
 
 <script>
@@ -13,8 +17,10 @@ export default {
     
 }
 </script>
+
 <style scoped>
 a {
     display: block;
 }
 </style>
+    
