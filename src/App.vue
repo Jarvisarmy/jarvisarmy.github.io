1aec73b968647e19b50986e3da730874e4e66d7f
Zeduo Zhang <jarviszhangzeduo@gmail.com>
2020-09-15 23:10:41 -0400

    totally update

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
     <div>
         <h2> hello </h2>
         <p> I am currently working my personal website using vue.js, the following links are the projects I done without using vue.js </p>
-        <a href="./../assets/tetris/tetris.html"> tetris </a>
-        <a href="./../assets/schedule/schedule.html"> schedule </a>
-        <a href="./../assets/color/colorGame.html"> colorGame </a>
-        
+        <h4> projects using web development skills such as CSS, HTML, JavaScript, bootstrap, etc </h4>
+        <a href="https://jarvisarmy.github.io/tetris"> tetris </a>
+        <a href="https://jarvisarmy.github.io/schedule/"> schedule </a>
+        <a href="https://jarvisarmy.github.io/color-game/"> colorGame </a>
+        <a href="https://jarvisarmy.github.io/dice-game/"> Pig Game </a>
+        <h4> game development projects using Unity3D </h4>
+        <a href="https://jarvisarmy.github.io/Flappy-Bird"> Flappy Bird </a>
+        <a href="https://jarvisarmy.github.io/Escape-game"> Escape Game </a>
     </div>
 
 
