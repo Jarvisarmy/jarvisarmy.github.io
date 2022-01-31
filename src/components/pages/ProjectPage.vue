90885217c4b77633040c9dd65da3d7897331c921
Jarvisarmy <33994261+Jarvisarmy@users.noreply.github.com>
2022-01-31 14:07:43 -0500

    Update ProjectPage.vue

--- src/components/pages/ProjectPage.vue
+++ src/components/pages/ProjectPage.vue
@@ -32,6 +32,44 @@
                 <a href="https://github.com/Jarvisarmy/BMI-Calculator-ios13"> BMICalculator </a>
             </li>
         </ul>
+        <h4> CPA805 website project (HTML, CSS, JavaScript, ReactJS, NodeJS, SQL) 2021
+        <ul> 
+            <li>
+                Heroku frontend: <a> https://still-thicket-95361.herokuapp.com/ </a>
+            </li>
+             <li>
+                Heroku backend: <a> https://guarded-anchorage-82080.herokuapp.com/ </a>
+            </li>
+             <li>
+                Github frontend: <a> https://github.com/Jarvisarmy/CAP805-frontend/tree/master </a>
+            </li>
+             <li>
+                Github backend: <a> https://github.com/Jarvisarmy/CAP805-backend/tree/master </a>
+            </li>
+            <li>
+                Focus on the whole process of building a project, including planning, system architecture, task management and bug tracking, database integration, testing, securing. Include authentication and authorization.
+            </li>
+        </ul>
+        <h4> CJV805 website project (Java, HTML, CSS, JavaScript, Mango DB) 2021
+        <ul>
+            <li>
+                Heroku frontend: <a> https://cjv-frontend.herokuapp.com/ </a>
+            </li>
+             <li>
+                Heroku backend: <a> https://cjv-backend.herokuapp.com/</a>
+            </li>
+             <li>
+                Github frontend: <a> https://github.com/Jarvisarmy/CJV-frontend </a>
+            </li>
+             <li>
+                Github backend: <a> https://github.com/Jarvisarmy/cjv-backend </a>
+            </li>
+            <li>
+                Building fake amazon website using strong skills of HTML, CSS and JavaScript. Extracting data from mango DB to display the content. Verifying the input to the login and register form.
+            </li>
+                
+            
+  
             
     </div>
 </template>
